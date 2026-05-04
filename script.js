81558bc15d33617df3ea79a971b09ecacb1acc34
masterphantom31-spec <masterphantom31@gmail.com>
2026-05-04 14:00:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function closeMenu(){
   document.getElementById("sidebar").style.left="-250px";
   document.getElementById("overlay").style.display="none";
 }
+
+/* 🔥 BANNER */
+let index = 0;
+const slides = document.getElementById("slides");
+const dots = document.querySelectorAll(".dot");
+
+function updateSlide() {
+  if (!slides) return;
+
+  slides.style.transform = "translateX(-" + index * 100 + "%)";
+
+  dots.forEach(d => d.classList.remove("active"));
+  if (dots[index]) dots[index].classList.add("active");
+}
+
+setInterval(() => {
+  if (!slides) return;
+  index = (index + 1) % dots.length;
+  updateSlide();
+}, 3000);
